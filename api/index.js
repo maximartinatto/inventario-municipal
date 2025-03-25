6dcfc8a289c89973e601d27d8a61d0c719b00dfe
maximartinatto <martinattomaximiliano@gmail.com>
2025-03-25 18:38:27 -0300

    port

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const PORT = process.env.PORT || 3001;
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('Api listening at 3001'); 
+  server.listen(PORT, () => {
+    console.log(`Server listenin in port: ${PORT}`); // eslint-disable-line no-console
   });
 }); 
